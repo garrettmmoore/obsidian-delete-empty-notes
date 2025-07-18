9db14d3c08dbefd6e68b9b608c531715aa478eba
Garrett Moore <28462557+garrettmmoore@users.noreply.github.com>
2025-07-18 07:50:29 -0700

    Add ability to delete untitled empty notes

--- main.ts
+++ main.ts
@@ -1,134 +1,63 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile, WorkspaceLeaf } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class AutoDeleteUntitledEmptyNotesPlugin extends Plugin {
+	private lastFile: TFile | null = null;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+	onload() {
+		this.registerEvent(
+			this.app.workspace.on('active-leaf-change', async (newLeaf: WorkspaceLeaf | null) => {
+				if (this.lastFile) {
+					await this.tryDeleteIfUntitledAndEmpty(this.lastFile);
 				}
-			}
-		});
+				this.lastFile = this.extractFileFromLeaf(newLeaf);
+			})
+		);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Handles closing the app or reloading
+		window.addEventListener('beforeunload', this.handleWindowClose);
+		this.register(() => window.removeEventListener('beforeunload', this.handleWindowClose));
 	}
 
 	onunload() {
-
+		// All cleanup handled by `register` and `registerEvent`
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	private handleWindowClose = async () => {
+		if (this.lastFile) {
+			await this.tryDeleteIfUntitledAndEmpty(this.lastFile);
+		}
+	};
+
+	private extractFileFromLeaf(leaf: WorkspaceLeaf | null): TFile | null {
+		if (!leaf) return null;
+		const view: any = leaf.view;
+		if (view && view.file instanceof TFile) {
+			return view.file as TFile;
+		}
+		return null;
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+	private async tryDeleteIfUntitledAndEmpty(file: TFile) {
+		// Only delete markdown files
+		if (file.extension !== 'md') return;
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+		// If file name contains 'Untitled'
+		if (!/untitled/i.test(file.basename)) return;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		// Check for empty content
+		const content = await this.app.vault.read(file);
+		if (content.trim().length > 0) return;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+		const filePath = file.path;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+		// Close open tabs for this file
+		this.app.workspace.iterateAllLeaves((leaf: WorkspaceLeaf) => {
+			const leafView: any = leaf.view;
+			if (leafView && leafView.file && leafView.file.path === filePath) {
+				leaf.detach();
+			}
+		});
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		await this.app.vault.delete(file);
 	}
 }
